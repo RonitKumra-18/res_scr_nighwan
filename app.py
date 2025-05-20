b9d788754dd54024c76938a72da3f1f3a88688ec
Ronit Kumra <ronitdev1881@proton.me>
2025-05-20 13:56:18 +0530

    Update app.py12

--- app.py
+++ app.py
@@ -139,4 +139,4 @@ ResumeMatcher.deploy()
 # ----------------- For Render -----------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=10000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080)
